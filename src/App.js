878b5b19467da81bafcd184dfbe2d0f64c6f8fdf
Pratham <bhagatpratham101@gmail.com>
2022-08-03 22:54:12 +0530

    added authentication with auth0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useAuth0 } from "@auth0/auth0-react";
+import "./App.css";
+import Logout from "./Components/Logout";
+import User from "./Components/User";
+import Login from "./Components/Login";
 
 function App() {
+  const { isAuthenticated } = useAuth0();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!isAuthenticated ? (
+        <div>
+          <p>Login.</p>
+          <Login />
+        </div>
+      ) : (
+        <div>
+          <Logout />
+          <User />
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      )}
     </div>
   );
 }
